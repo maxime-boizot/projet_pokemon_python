e086ede5f18784966c48e9765fe90b8ad5d804a6
ogb4n <ogb4n@protonmail.com>
2023-11-24 18:43:42 +0100

    dedicated_folder

--- functions/interactionsLines.py
+++
@@ -1,7 +0,0 @@
-initCombat = "Cynthia vous défie en combat pour le titre de champion ! "
-
-combat1 = "Cynthia prépare son premier pokemon ! Qui voulez-vous envoyer au combat ?"
-
-cynthiaEntersFight = "Cynthia entre sur le terrain ! "
-
-combat1 = "Cynthia prépare son premier pokemon ! Qui voulez-vous envoyer au combat ?"
