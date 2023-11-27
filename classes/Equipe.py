a7f9696269df33b75eceb9378aa4cff288cb0f89
Maxance Ferran <maxance.ferran@ynov.com>
2023-11-27 10:04:51 +0100

    changepokemon fonctionnel

--- classes/Equipe.py
+++ classes/Equipe.py
@@ -29,7 +29,7 @@ if __name__ == "__main__":
 
     print('-----------------------------------------')
 
-    monekip.changepokemon(Pikachu)
+    monekip.changepokemon("Pikachu")
 
     print(monekip)
         
