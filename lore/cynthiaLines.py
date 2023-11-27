e385853586891e1b3391c315c6066ac85589ae88
Maxance Ferran <maxance.ferran@ynov.com>
2023-11-27 11:10:40 +0100

    afterfight translate

--- lore/cynthiaLines.py
+++ lore/cynthiaLines.py
@@ -10,4 +10,10 @@ combat2 = "Je ne me rappelle pas la dernière fois que j'ai été dans une situa
 
 cynthiaDefeat = "Je ne peux pas croire que j'ai perdu ! Mais quelle belle bataille !"
 
-afterfight = "C'était excellent. Vraiment, une bataille exceptionnelle. Vous avez donné le soutien dont vos Pokémon avaient besoin pour maximiser leur puissance. Et vous les avez guidés avec certitude pour assurer la victoire. Vous avez à la fois de la passion et de la fraîcheur calculatrice. Ensemble, vous et votre Pokémon pouvez surmonter tous les défis qui se présentent à vous. Ce sont les impressions que j'ai eues de notre combat. Je suis heureux d'avoir participé au couronnement du nouveau champion de Sinnoh ! Venez avec moi. Nous prendrons l'ascenseur."
+afterfight = """
+C'était excellent. Vraiment, une bataille exceptionnelle.
+Vous avez donné le soutien dont vos Pokémon avaient besoin pour maximiser leur puissance. 
+Et vous les avez guidés avec certitude pour assurer la victoire. Vous avez à la fois de la passion et de la fraîcheur calculatrice. 
+Ensemble, vous et votre Pokémon pouvez surmonter tous les défis qui se présentent à vous. 
+Ce sont les impressions que j'ai eues de notre combat. Je suis heureux d'avoir participé au couronnement du nouveau champion de Sinnoh ! 
+Venez avec moi. Nous prendrons l'ascenseur."""
